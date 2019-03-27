bd1596ba32a27c343f6189717aefaaaef162cdff
chmnata <46324452+chmnata@users.noreply.github.com>
2019-03-27 17:05:12 -0400

    update columns

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,10 +1,10 @@
-CREATE TABLE gis_shared_streets.platonic_ideal_table
+CREATE TABLE gis_shared_streets.output_table_structure
 (
     reference_id text COLLATE pg_catalog."default",
     from_intersection text COLLATE pg_catalog."default",
     to_intersection text COLLATE pg_catalog."default",
     road_class text COLLATE pg_catalog."default",
-    direction text COLLATE pg_catalog."default",
+    ss_direction text COLLATE pg_catalog."default",
     reference_length numeric,
     side text COLLATE pg_catalog."default",
     section numrange,
@@ -17,12 +17,12 @@ WITH (
 )
 TABLESPACE pg_default;
 
-ALTER TABLE gis_shared_streets.platonic_ideal_table
+ALTER TABLE gis_shared_streets.output_table_structure
     OWNER to natalie;
 
-GRANT SELECT ON TABLE gis_shared_streets.platonic_ideal_table TO bdit_humans;
+GRANT SELECT ON TABLE gis_shared_streets.output_table_structure TO bdit_humans;
 
-GRANT ALL ON TABLE gis_shared_streets.platonic_ideal_table TO natalie;
+GRANT ALL ON TABLE gis_shared_streets.output_table_structure TO natalie;
 
-COMMENT ON TABLE gis_shared_streets.platonic_ideal_table
+COMMENT ON TABLE gis_shared_streets.output_table_structure
     IS 'The ideal table structure for all sharedstreets returned match results.';
